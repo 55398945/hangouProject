c640a95b34235043b7c61785ab38e2570eb419fa
yaya <330391379@qq.com>
2017-07-24 14:38:22 +0800

    change login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,8 +10,8 @@
     
     <!--账户密码-->
     <div class="login-num" >
-    	<input type="text" placeholder="请输入用户名/已验证手机"/>
-    	<input type="text" placeholder="请输入登录密码" />
+    	<p>账 户 <input type="text" placeholder="请输入用户名/已验证手机"/><i class="iconfont iconfont-router">&#xe654;</i></p>
+    	<p>密 码 <input type="text" placeholder="请输入登录密码" /><i class="iconfont iconfont-router">&#xe654;</i></p>
     </div>
     
     
@@ -43,16 +43,38 @@ export default {
 }
 .login-header i{
 	color: #737373;
-	font-size: 1.2rem;
+	font-size: 1rem;
 	padding-left: 1rem;
 }
 .login-header-login{
-	font-size: 1rem;
+	font-size: 0.8rem;
 	color: #111;
 }
 .login-header-register{
 	color: #ed5564;
-	font-size: 0.8rem;
+	font-size: 0.7rem;
 	padding-right: 1rem;
+}
+
+/*账户密码*/
+.login-num{
+	font-size: 0.6rem;
+	color: #444;
+	background: #fff;
+}
+.login-num p{
+	height: 1.95rem;
+	border-bottom: 0.05rem solid #eee;
+	margin-left: 1rem;
+  line-height: 1.95rem;
+}
+.login-num p input{
+	border: none;
+	width: 70%;
+	margin-left: 1rem;
+}
+.login-num p i{
+	font-size: 0.2rem;
+	color: #969971;
 }
 </style>
